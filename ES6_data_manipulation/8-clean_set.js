51e2c6e7cf74916a4b493961b9f1eacc8f426b59
Zineb749 <zsakho58@gmail.com>
2025-04-02 17:08:30 +0200

    tasks and readme

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, string) {
+  if (string === '') {
+    return '';
+  }
+  const result = Array.from(set)
+    .filter((item) => item.startsWith(string))
+    .map((item) => item.slice(string.length));
+    
+  return result.join('-');
+}
